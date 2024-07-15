6c3784ec1f78721654e02de5521350b61d432294
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-07-16 00:06:47 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// Fix the code in the code tab to pass this challenge (only syntax errors).
+
+// function squaed(b) {
+// 	return a * a
+// }
+
+function squaed(b) {
+	return b * b
+}
+
+console.log(squaed(5));
+console.log(squaed(9));
+console.log(squaed(100));
